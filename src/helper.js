93ceb6c751c35ff5ee7e2086cfe61bbc07596493
redaBenchraa <redabenchraa@gmail.com>
2020-12-13 05:00:30 +0100

    Document helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,18 @@
-String.prototype.formatUnicorn =
-  String.prototype.formatUnicorn ||
-  function () {
-    "use strict";
-    var str = this.toString();
-    if (arguments.length) {
-      var t = typeof arguments[0];
-      var key;
-      var args = "string" === t || "number" === t ? Array.prototype.slice.call(arguments) : arguments[0];
+// Source https://gist.github.com/richardblondet/9a23a155e6a784f1ff2cbd8d84accd72
+String.prototype.formatUnicorn = String.prototype.formatUnicorn || function () {
+  "use strict";
+  var str = this.toString();
+  if (arguments.length) {
+    var t = typeof arguments[0];
+    var key;
+    var args = ("string" === t || "number" === t) ?
+      Array.prototype.slice.call(arguments)
+      : arguments[0];
 
-      for (key in args) {
-        str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
-      }
+    for (key in args) {
+      str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
     }
+  }
 
-    return str;
-  };
+  return str;
+};
